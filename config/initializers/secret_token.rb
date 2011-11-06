99371922b0a180d5c35ef6d9a6a6a81b5746c5f1
sarunnio <sarunnio@hotmail.com>
2011-11-06 17:06:13 +0700

    initial comment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pepe::Application.config.secret_token = 'cced32724d1cf4e907f5588504d618a90beb1ffb988b3c5cd49e9d1b69887da59070bef76204534a89237e7853441811477e728379f981b378848904db4bf807'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Pepe::Application.config.session_store :cookie_store, key: '_pepe_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Pepe::Application.config.session_store :active_record_store
